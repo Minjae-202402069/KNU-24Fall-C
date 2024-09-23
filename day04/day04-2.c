5483c8b6d0320cd9bdbe8815d5bfe40dc7c1c8f9
Wonjae Lee-202402070 <leewj4656@gmail.com>
2024-09-23 17:33:27 +0900

    포인터_함수호출

---
+++ day04/day04-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int *num1, int *num2);
+
+int main()
+{
+	int a = 10, b = 20;
+
+	printf("a = %d, b = %d\n", a, b);
+	swap(&a, &b);
+	printf("a = %d , b= %d\n", a, b);
+
+	return 0;
+}
+
+void swap(int  *num1, int *num2)
+{
+	int temp;
+	temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}

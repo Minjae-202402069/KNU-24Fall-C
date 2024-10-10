aca78a0b3a52575173f66c72e59485473e0425f0
Minjae-202402069 <lmjj0208@naver.com>
2024-10-10 13:23:01 +0900

    Update day07-1.c

--- day07/day07-1.c
+++ day07/day07-1.c
@@ -2,13 +2,15 @@
 
 int main()
 {
-	int n = 3;
-	
+	int n;
+	printf("피라미드 층수 설정:  ");
+	scanf_s("%d", &n);
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n-i; j++) {
 			printf(" ");
 		}
-		for (int k = 0; k < i*2+1; k++) {
+		for (int k = 0; k < ( i*2 )+1; k++) {
 			printf("*");
 		}
 		printf("\n");
